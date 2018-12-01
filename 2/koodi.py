b2c4586b642a11df5c3e87f2ded2138b6efca2dd
aapalo <aake.paloniemi@aalto.fi>
2018-12-01 13:06:59 +0200

    Day 1

--- 2/koodi.py
+++ 2/koodi.py
@@ -1,33 +1,51 @@
-pva = 1
-
 #from collections import Counter
 #import re
-import os
+#import os
+import time
 
 '''     #######     '''
 
+#part 1
 def day(te):
+
     return 0
 
+#part 2
 def dayb(te):
-    return 1
 
+    return 0
 
 '''     #######     '''
-dev = 1
-b = 0
-samp = 1
 
-t = ""
+pva = 2 #which day
+dev = 0 # extra prints
+part = 1 # 1,2 or 3
+samp = 1 # 0 or 1
+
+time0 = time.time()
+
 if samp == 1:
-    with open(str(pva) + "/sample.txt","r") as f:
-        t = f.readlines()
-elif samp == 0:
-    with open(str(pva) + "/input.txt","r") as f:
-        t = f.readlines()
-t = [x.strip().replace('<->','').replace(',','').replace('  ',' ') for x in t]
-
-if b == 1:
-    print(dayb(t))
+    filename = "/sample.txt"
 else:
-    print(day(t))
+    filename = "/input.txt"
+
+try:
+    with open(str(pva) + filename,"r") as f:
+        t = f.readlines()
+except FileNotFoundError:
+    with open("." + filename,"r") as f:
+        t = f.readlines()
+
+t = [int(x.strip().replace('<->','').replace(',','').replace('  ',' ')) for x in t]
+
+if part == 1:
+    print("Part 1: ", day(t))
+elif part == 2:
+    print("Part 2: ", dayb(t))
+elif part == 3:
+    #run both
+    print("Part 1: ", day(t))
+    print("Part 2: ", dayb(t))
+
+tdif = time.time() - time0
+print("Elapsed time: {:.4f} s".format(tdif))
