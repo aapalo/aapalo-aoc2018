ce65da1ee5e306e33af4ae5cdbd2b38d06ebfaa2
aapalo <aake.paloniemi@aalto.fi>
2018-12-01 14:58:54 +0200

    Day 1 with set

--- 1/koodi.py
+++ 1/koodi.py
@@ -1,4 +1,4 @@
-pva = 1
+
 
 #from collections import Counter
 #import re
@@ -31,10 +31,28 @@ def dayb(te):
     #print(len(lsum),sum)
     return sum
 
-'''     #######     '''
+#part 2, with set()
+def dayb2(te):
+    sum = 0
+    lsum = set()
+    ans = False
+    while(1):
+        for t in te:
+            #print(l)
+            sum += t
+            if sum in lsum:
+                ans = True
+                break
+            lsum.add(sum)
+        if ans == True:
+            break
+    #print(len(lsum),sum)
+    return sum
 
+'''     #######     '''
+pva = 1
 dev = 0 # extra prints
-part = 1 # 1,2 or 3
+part = 2 # 1,2 or 3
 samp = 0 # 0 or 1
 
 time0 = time.time()
@@ -56,7 +74,7 @@ t = [int(x.strip().replace('<->','').replace(',','').replace('  ',' ')) for x in
 if part == 1:
     print("Part 1: ", day(t))
 elif part == 2:
-    print("Part 2: ", dayb(t))
+    print("Part 2: ", dayb2(t))
 elif part == 3:
     #run both
     print("Part 1: ", day(t))
--- README.md
+++ README.md
@@ -7,9 +7,10 @@ My solutions for [Advent of code](https://adventofcode.com/2018) programming puz
 Used time.time() to measure the time difference in seconds.
 Running CPython via terminal and Atom took the same time, so values for Atom are shown.
 
-| Program  | Atom     | pypy3   | Atom/pypy3 |
-| -------- | :------: | :-----: | :--------: |
-| 1-1      | 0.001    | 0.005   | 0.2        |
-| 1-2      | 105      | 4.4     | 25.9       |
-| 2-1      |     |    |     |
-| 2-2      |     |    |     |
+| Program     | Atom     | pypy3   | Atom/pypy3 |
+| --------    | :------: | :-----: | :--------: |
+| 1-1         | 0.001    | 0.005   | 0.2        |
+| 1-2, w/ list| 105      | 4.4     | 25.9       |
+| 1-2, w/ set | 0.03     | 0.018   | 1.67       |
+| 2-1         |     |    |     |
+| 2-2         |     |    |     |
