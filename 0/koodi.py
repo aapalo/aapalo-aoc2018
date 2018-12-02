e008c8515fcf87a448964bf2e2adfc7eede154c3
Aake Paloniemi <aake.paloniemi@aalto.fi>
2018-12-02 13:26:00 +0200

    Day 2

--- 0/koodi.py
+++ 0/koodi.py
@@ -1,23 +1,22 @@
-pva = 1
-
 #from collections import Counter
 #import re
 #import os
 import time
 '''     #######     '''
 
-#part 1
+''' Part 1 '''
 def day(te):
 
     return 0
 
-#part 2
+''' Part 2 '''
 def dayb(te):
 
     return 0
 
 '''     #######     '''
 
+pva = 26
 dev = 0 # extra prints
 part = 1 # 1,2 or 3
 samp = 1 # 0 or 1
@@ -36,7 +35,7 @@ except FileNotFoundError:
     with open("." + filename,"r") as f:
         t = f.readlines()
 
-t = [int(x.strip().replace('<->','').replace(',','').replace('  ',' ')) for x in t]
+t = [(x.strip().replace('<->','').replace(',','').replace('  ',' ')) for x in t]
 
 if part == 1:
     print("Part 1: ", day(t))
--- 2/koodi.py
+++ 2/koodi.py
@@ -1,26 +1,69 @@
-#from collections import Counter
+from collections import Counter
 #import re
 #import os
 import time
-
 '''     #######     '''
 
-#part 1
+''' Part 1 '''
 def day(te):
+    threes = 0
+    twos = 0
+    tw = 0
+    th = 0
+    for t in te:
+        c = Counter(t)
+        matches = 0
+        for i in c.keys():
+            if (c[i]) == 2:
+                tw += 1
+            elif (c[i]) == 3:
+                th += 1
+        if tw > 0:
+            twos += 1
+        if th > 0:
+            threes += 1
+        tw = 0
+        th = 0
+        #print(twos,threes,twos*threes)
+    return twos*threes
 
-    return 0
+''' Part 2 '''
+def strdiff(a,b):
+    d = 0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            d += 1
+    return d
 
-#part 2
+def strcomm(a,b):
+    s = ""
+    for i in range(len(a)):
+        if a[i] == b[i]:
+            s += a[i]
+    return s
+
 def dayb(te):
+    answ = False
 
+    for t in range(len(te)):
+        foo = te[t]
+        for i in range(len(te)):
+            if t == i:
+                continue
+            goo = te[i]
+            d = strdiff(foo,goo)
+            if d == 1:
+                print(foo,goo,d)
+                com = strcomm(foo,goo)
+                return com
     return 0
 
 '''     #######     '''
 
 pva = 2 #which day
 dev = 0 # extra prints
-part = 1 # 1,2 or 3
-samp = 1 # 0 or 1
+part = 2 # 1,2 or 3
+samp = 0 # 0 or 1
 
 time0 = time.time()
 
@@ -36,7 +79,7 @@ except FileNotFoundError:
     with open("." + filename,"r") as f:
         t = f.readlines()
 
-t = [int(x.strip().replace('<->','').replace(',','').replace('  ',' ')) for x in t]
+t = [(x.strip().replace('<->','').replace(',','').replace('  ',' ')) for x in t]
 
 if part == 1:
     print("Part 1: ", day(t))
