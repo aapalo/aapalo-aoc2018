7a01f92941d379302fcf445c6514d70e4062dc64
aapalo <aake.paloniemi@aalto.fi>
2018-12-07 09:52:09 +0200

    Day 7 initial

---
+++ 7/koodi.py
@@ -0,0 +1,64 @@
+#from collections import Counter
+#import re
+#import os
+import time
+'''     #######     '''
+
+''' Part 1 '''
+def day(te):
+    for t in te:
+        prev = t.split(" ")[1]
+        next = t.split(" ")[7]
+        print(prev,ord(prev),next,ord(next))
+    #find start and end steps?
+    #for each step, add all availables in a list
+    #go through availables in alphabetical order
+    
+    '''
+        for tasks in available:
+            if ord(a) < ord(b):
+                do_stuff_with(a)
+                pop(a)
+    '''
+
+    return 0
+
+''' Part 2 '''
+def dayb(te):
+
+    return 0
+
+'''     #######     '''
+
+pva = 7
+dev = 0 # extra prints
+part = 1 # 1,2 or 3
+samp = 1 # 0 or 1
+
+time0 = time.time()
+
+if samp == 1:
+    filename = "/sample.txt"
+else:
+    filename = "/input.txt"
+
+try:
+    with open(str(pva) + filename,"r") as f:
+        t = f.readlines()
+except FileNotFoundError:
+    with open("." + filename,"r") as f:
+        t = f.readlines()
+
+t = [(x.strip().replace('<->','').replace(',','').replace('  ',' ')) for x in t]
+
+if part == 1:
+    print("Part 1: ", day(t))
+elif part == 2:
+    print("Part 2: ", dayb(t))
+elif part == 3:
+    #run both
+    print("Part 1: ", day(t))
+    print("Part 2: ", dayb(t))
+
+tdif = time.time() - time0
+print("Elapsed time: {:.4f} s".format(tdif))
